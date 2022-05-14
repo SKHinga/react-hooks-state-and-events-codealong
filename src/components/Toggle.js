ab507c91843b6c97b76c5952f1053714aeea99c5
Samuel Hinga <samuelhinga200@gmail.com>
2022-05-14 16:04:05 +0300

    Assignment completed

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const[turn, setTurn] = useState(false)
+
+  function handleTurn(){
+    setTurn(turn=>!turn)
+  }
+
+  const color = turn ? "red" : "white"
+
+  return <button style={{background:color}} onClick={handleTurn}>{turn ? "On" : "Off"}</button>;
 }
 
 export default Toggle;
